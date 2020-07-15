41ec32c54dfd7e79724602121d5a7adb62310795
silviyaejas <silviyakvm@gmail.com>
2020-07-15 18:47:52 +0530

    setting display screen

--- python_calculator.py
+++ python_calculator.py
@@ -4,5 +4,14 @@ from tkinter import *
 window=Tk()
 window.geometry("400x500+500+150")
 window.title("Calculator")
-window.configure(bg='black')
+window.configure(background="black")
+#creating diaplay screen
+display_screen = Label(window, text="0", font=("Serif", 40), fg="white", bg="#1F2333", anchor=NE)
+
+
+
+
+
+display_screen.place(x=25, y=30, width=350, height=80)
+
 window.mainloop()
