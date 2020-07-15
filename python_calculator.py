e281439467665962d5a0131c8779f6e9db1c8ade
silviyaejas <silviyakvm@gmail.com>
2020-07-15 15:55:40 +0530

    setting window for calculator

--- python_calculator.py
+++ python_calculator.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+#creating window for calculator
 window=Tk()
-
+window.geometry("400x500+500+150")
+window.title("Calculator")
+window.configure(bg='black')
 window.mainloop()
