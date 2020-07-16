55b728f10764df75145acb38219e2622d2e26adb
silviyaejas <silviyakvm@gmail.com>
2020-07-16 07:10:11 +0530

    defined function that to be performed while clicking any button except clear and equal to buttons

--- python_calculator.py
+++ python_calculator.py
@@ -1,23 +1,43 @@
 from tkinter import *
 
-
-#creating window for calculator
 from tkinter import Button
+
+# commands for buttons
+
+def btn_clicked(value):
+    old_value = label_text.get()
+    if old_value == "Error":
+        label_text.set(str(value))
+    else:
+        label_text.set(old_value+str(value))
+
+
+# creating window for calculator
 
 window=Tk()
 window.geometry("+500+150")
 window.title("Calculator")
 window.configure(background="black")
-#creating diaplay screen
+
+
+
+# creating diaplay screen
+
+label_text = StringVar()
+
 display_screen = Label(window,
-                       text="0",
                        font=("Serif", 40),
+                       textvariable=label_text,
                        pady=20,
                        fg="white",
                        bg="#1F2333",
                        borderwidth=20,
                        relief="sunken",
                        anchor=E)
+
+
+# buttons in calculator
+
 btn_clr = Button(window,
              text="C",
              width=4,
@@ -27,87 +47,104 @@ btn_open = Button(window,
              text="(",
              width=4,
              bg="#434B69", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked("("))
 btn_close = Button(window,
              text=")",
              width=4,
              bg="#434B69", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(")"))
 btn_div = Button(window,
                text="/",
                width=4,
                bg="#434B69", fg="white",
-               font=("Serif 24 bold"))
+               font=("Serif 24 bold"),
+               command=lambda: btn_clicked("/"))
 btn_7 = Button(window,
              text="7",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(7) )
 btn_8 = Button(window,
              text="8",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(8))
 btn_9 = Button(window,
              text="9",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(9))
 btn_mul = Button(window,
                text="x",
                width=4,
                bg="#434B69", fg="white",
-               font=("Serif 24 bold"))
+               font=("Serif 24 bold"),
+               command=lambda: btn_clicked("*"))
 btn_4 = Button(window,
              text="4",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(4))
 btn_5 = Button(window,
              text="5",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(5))
 btn_6 = Button(window,
              text="6",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(6))
 btn_minus = Button(window,
                text="-",
                width=4,
                bg="#434B69", fg="white",
-               font=("Serif 24 bold"))
+               font=("Serif 24 bold"),
+               command=lambda: btn_clicked("-"))
 btn_1 = Button(window,
              text="1",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(1))
 btn_2 = Button(window,
              text="2",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(2))
 btn_3 = Button(window,
              text="3",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(3))
 btn_plus = Button(window,
                text="+",
                width=4,
                bg="#434B69", fg="white",
-               font=("Serif 24 bold"))
+               font=("Serif 24 bold"),
+               command=lambda: btn_clicked("+"))
 btn_0 = Button(window,
              text="0",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked(0))
 btn_dot = Button(window,
              text=".",
              width=4,
              bg="#141933", fg="white",
-             font=("Serif 24 bold"))
+             font=("Serif 24 bold"),
+             command=lambda: btn_clicked("."))
 btn_equal = Button(window,
              text="=",
              width=4,
@@ -116,6 +153,7 @@ btn_equal = Button(window,
 
 
 
+# positions for buttons and display screen
 
 display_screen.grid(row=0, columnspan=4, sticky="nsew")
 btn_clr.grid(row=1, column=0)
@@ -137,4 +175,7 @@ btn_plus.grid(row=4, column=3)
 btn_0.grid(row=5, column=0, columnspan=2, sticky="snew")
 btn_dot.grid(row=5, column=2)
 btn_equal.grid(row=5, column=3)
+
+
+
 window.mainloop()
